c7d857b55f064655580a4c3d69bcc68609998e23
xieyajie <xyjxyf@163.com>
2026-02-27 10:57:19 +0800

    更新部署脚本

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,16 @@ cd dist
 
 # 初始化新的Git仓库
 git init
+git checkout -B gh-pages
 git add -A
-git commit -m "Deploy to GitHub Pages"
+
+if git diff --cached --quiet; then
+	echo "无变更可提交，直接推送。"
+else
+	git commit -m "Deploy to GitHub Pages"
+fi
 
 # 推送到gh-pages分支
-git push -f https://github.com/FreedomSnow/braveray.git master:gh-pages
+git push -f https://github.com/FreedomSnow/braveray.git gh-pages
 
 cd -
